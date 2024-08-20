81e1d1329b2db3255e51a6d73b1138eb8d5c231c
梶塚太智 <kajizukataichi@outlook.jp>
2024-08-21 06:24:34 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,92 @@
 use std::{collections::HashMap, io, io::Write, process::Command};
 
+fn main() {
+    println!("Nashe");
+    let mut nashe = Nashe {
+        memory: HashMap::new(),
+    };
+    loop {
+        let user_code = input("> ");
+        if user_code.is_empty() {
+            continue;
+        }
+        println!("{}", nashe.run_command(user_code));
+    }
+}
+
+/// Get standard input
+fn input(prompt: &str) -> String {
+    print!("{}", prompt);
+    io::stdout().flush().unwrap();
+    let mut result = String::new();
+    io::stdin().read_line(&mut result).ok();
+    result.trim().to_string()
+}
+
 struct Nashe {
     memory: HashMap<String, String>,
 }
 
 impl Nashe {
-    fn eval_code(&mut self, code: String) {
-        let mut splited: Vec<&str> = code.split("=").collect();
-        if splited.len() == 1 {
-            println!("{}", self.run_command(splited[0].to_string()));
+    fn run_command(&mut self, code: String) -> String {
+        let code: Vec<String> = self.parse_args(code);
+        let args = if code.len() < 2 {
+            vec![]
         } else {
-            let identify: String = splited[0].trim().to_string();
-            let result = self.run_command({
-                splited.remove(0);
-                splited.join("=")
-            });
-            self.memory.insert(identify, result);
+            code[1..code.len()].to_vec()
+        };
+
+        if code[0] == "cd" {
+            if std::env::set_current_dir(if let Some(i) = args.get(0) {
+                i
+            } else {
+                return "".to_string();
+            })
+            .is_err()
+            {
+                return format!("cd: {}: no such file or directory\n", args[0]);
+            } else {
+                return "".to_string();
+            };
+        }
+        if code[0] == "var" {
+            self.memory.insert(
+                if let Some(i) = args.get(0) {
+                    i.to_owned()
+                } else {
+                    return "".to_string();
+                },
+                if let Some(i) = args.get(1) {
+                    i.to_owned()
+                } else {
+                    return "".to_string();
+                },
+            );
+            return "".to_string();
+        }
+
+        let result = Command::new(code[0].to_string()).args(args).output();
+        if let Ok(output) = result {
+            if output.status.success() {
+                let stdout = String::from_utf8_lossy(&output.stdout);
+                format!("{}", stdout.trim())
+            } else {
+                let stderr = String::from_utf8_lossy(&output.stderr);
+                format!("{}", stderr.trim())
+            }
+        } else {
+            code.join("\n")
         }
     }
 
     fn parse_args(&mut self, source: String) -> Vec<String> {
-        fn tokenize_args(input: String) -> Vec<String> {
+        let tokens = {
             let mut tokens = Vec::new();
             let mut current_token = String::new();
             let mut in_parentheses: usize = 0;
             let mut in_quote = false;
 
-            for c in input.chars() {
+            for c in source.chars() {
                 match c {
                     '(' if !in_quote => {
                         if in_parentheses != 0 {
@@ -49,8 +109,6 @@ impl Nashe {
                                 tokens.push(current_token.clone());
                                 current_token.clear();
                             }
-                        } else {
-                            panic!("チノちゃん「うるさいですね...」");
                         }
                     }
                     '"' => {
@@ -84,21 +142,13 @@ impl Nashe {
                 }
             }
 
-            if in_parentheses != 0 {
-                panic!("チノちゃん「うるさいですね...」");
-            }
-            if in_quote {
-                panic!("チノちゃん「うるさいですね...」");
-            }
-
             if !current_token.is_empty() {
                 tokens.push(current_token);
             }
 
             tokens
-        }
+        };
 
-        let tokens = tokenize_args(source);
         let mut result: Vec<String> = vec![];
         for token in tokens {
             let token = token.trim().to_string();
@@ -117,63 +167,5 @@ impl Nashe {
             }
         }
         result
-    }
-
-    fn run_command(&mut self, code: String) -> String {
-        let code: Vec<String> = self.parse_args(code);
-        let args = if code.is_empty() {
-            vec![]
-        } else {
-            code[1..code.len()].to_vec()
-        };
-        if code[0] == "cd" {
-            if std::env::set_current_dir(if let Some(i) = args.get(0) {
-                i
-            } else {
-                return "".to_string();
-            })
-            .is_err()
-            {
-                return format!("cd: {}: no such file or directory\n", args[0]);
-            } else {
-                return "".to_string();
-            };
-        }
-
-        let result = Command::new(code[0].to_string()).args(args).output();
-        if let Ok(output) = result {
-            if output.status.success() {
-                let stdout = String::from_utf8_lossy(&output.stdout);
-                format!("{}", stdout.trim())
-            } else {
-                let stderr = String::from_utf8_lossy(&output.stderr);
-                format!("{}", stderr.trim())
-            }
-        } else {
-            code.join("\n")
-        }
-    }
-}
-
-/// Get standard input
-fn input(prompt: &str) -> String {
-    print!("{}", prompt);
-    io::stdout().flush().unwrap();
-    let mut result = String::new();
-    io::stdin().read_line(&mut result).ok();
-    result.trim().to_string()
-}
-
-fn main() {
-    println!("Nashe");
-    let mut nashe = Nashe {
-        memory: HashMap::new(),
-    };
-    loop {
-        let user_code = input("> ");
-        if user_code.is_empty() {
-            continue;
-        }
-        nashe.eval_code(user_code)
     }
 }
